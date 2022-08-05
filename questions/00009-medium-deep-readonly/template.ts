2af9042fa8e4e1f591f434d6ff95807480d98c37
cher2018 <1154110064@qq.com>
2022-08-05 13:52:26 +0800

    用法: keyof T extends never

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,6 @@
-type DeepReadonly<T> = any
+// never 是一个特殊的联合类型，非对象T如果用keyof T extends never 就相当于一个空的联合类型是否能继承never
+type DeepReadonly<T> = keyof T extends never
+  ? T
+  : { readonly [K in keyof T]: DeepReadonly<T[K]> }
+
+// type DeepReadonly<T> = T extends object ? { readonly [K in keyof T]: DeepReadonly<T[K]> } : T
