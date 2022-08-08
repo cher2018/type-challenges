7a389d9be0f9f7a4bd841efbf3c4132414e25735
cher2018 <1154110064@qq.com>
2022-08-08 09:41:24 +0800

    0012的题目挺有趣的

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,13 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T = {}> = {
+  option: <K extends string, V>(key: K extends keyof T
+    ? V extends T[K] ? never : K
+    : K,
+    value: V) => Chainable<Omit<T, K> & Record<K, V>>
+  get: () => T
 }
+
+// 详细解析看这里 https://github.com/type-challenges/type-challenges/issues/13951
+// 知识点：
+// 1. 使用 T = {} 作为默认值
+// 2. 通过递归传递Chainable<T>可实现递归全局记录
+// 3. 注意第三个例子，相同key值的时候需要替换value值
